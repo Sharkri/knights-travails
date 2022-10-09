a9f4454cfe98db0ba033efc5795549d7f1963dd1
Sharkri <sharkdude955@gmail.com>
2022-10-08 21:27:49 -0500

    add shortest knight path searcher

---
+++ driver.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+import knightMoves from "./knightTravails.js";
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+function getRandomMove() {
+  const start = [getRandomNumber(7), getRandomNumber(7)];
+  const end = [getRandomNumber(7), getRandomNumber(7)];
+  return { start, end };
+}
+
+const { start, end } = getRandomMove();
+knightMoves(start, end);
+knightMoves(start, end, (path) => console.log(path));
--- knightTravails.js
+++ knightTravails.js
@@ -1,25 +1,76 @@
-function Game() {
-  const gameboard = [];
+function Board() {
+  const gameboard = Array(8)
+    .fill()
+    .map(() =>
+      Array(8)
+        .fill()
+        .map(() => null)
+    );
+  gameboard.reverse();
 
-  for (let i = 0; i < 8; i += 1) {
-    const row = [];
-    for (let j = 0; j < 8; j += 1) row.push(null);
-    gameboard.push(row);
+  function getGameBoard(coords) {
+    return coords ? gameboard[coords[0]][coords[1]] : gameboard;
   }
 
-  const getGameBoard = () => gameboard;
-
-  function placePiece(coords, piece) {
-    const x = coords[0];
-    const y = coords[1];
-    if (x >= 8 || y >= 8) return;
-    gameboard[x][y] = piece;
+  function getPossibleKnightMoves(start) {
+    const row = start[1];
+    const col = start[0];
+    if (row > 7 || col > 7 || row < 0 || col < 0) return null;
+    const possibleMoves = [
+      [col + 2, row - 1],
+      [col + 2, row + 1],
+      [col + 1, row - 2],
+      [col + 1, row + 2],
+      [col - 1, row + 2],
+      [col - 1, row - 2],
+      [col - 2, row - 1],
+      [col - 2, row + 1],
+    ];
+    // Filter moves that are off the board
+    return possibleMoves.filter(
+      (move) => Math.min(...move) >= 0 && Math.max(...move) < 8
+    );
   }
-
-  return { getGameBoard, placePiece };
+  return { getGameBoard, getPossibleKnightMoves };
+}
+function shortestKnightPath(start, end) {
+  const board = Board();
+  const queue = [{ coords: start, predecessor: null }];
+  const path = [];
+  while (queue.length) {
+    // dequeue first in queue
+    const first = queue.shift();
+    // check if reached the endpoint. need to stringify to compare arrays
+    if (JSON.stringify(first.coords) === JSON.stringify(end)) {
+      let temp = first;
+      while (temp) {
+        path.push(temp.coords);
+        temp = temp.predecessor;
+      }
+      break;
+    }
+    // enqueue possible moves
+    const possibleMoves = board.getPossibleKnightMoves([
+      first.coords[0],
+      first.coords[1],
+    ]);
+    possibleMoves.forEach((possibleMove) =>
+      queue.push({ coords: possibleMove, predecessor: first })
+    );
+  }
+  return path.reverse();
 }
 
-function knightMoves(start, end) {
-  const game = Game();
-  game.placePiece(start, "knight");
+function knightMoves(start, end, fn) {
+  if (Math.min(...start, ...end) < 0 || Math.max(...start, ...end) > 7) {
+    return null;
+  }
+  const path = shortestKnightPath(start, end);
+  if (typeof fn === "function") fn(path);
+  else {
+    console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+    path.map((move) => console.log(move));
+  }
+  return path;
 }
+export default knightMoves;
